56c9437a79d60742912f5fd7afd6cec2a155ea31
GuikiPT <foxzillapt17@gmail.com>
2023-03-01 14:55:08 +0000

    Added better-logging config

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 // Node Modules
 const Discord = require('discord.js');
+const colors = require('colors/safe');
+const moment = require('moment');
 require('dotenv').config();
-
+// https://olian04.gitbook.io/better-logging/
+require('better-logging')(console, {
+    format: ctx => `[${moment().format('HH:mm:ss')}] [${moment().format('L')}] ${ctx.type} >> ${ctx.msg}`,
+    saveToFile: `${__dirname}/logs/${moment().format('YYYY')}/${moment().format('M')}/${moment().format('D')}.log`,
+    color: {
+        base: colors.grey,
+        type: {
+            debug: colors.green,
+            info: colors.white,
+            log: colors.grey,
+            error: colors.red,
+            warn: colors.yellow,
+        }
+    },
+});
 
 // Client Configuration
 const client = new Discord.Client({
