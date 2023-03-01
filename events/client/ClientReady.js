7befc29ca09030224dcd38b5f37dbe30fd1294ff
GuikiPT <foxzillapt17@gmail.com>
2023-03-01 14:58:29 +0000

    Added discord presence status to bot

--- events/client/ClientReady.js
+++ events/client/ClientReady.js
@@ -12,5 +12,11 @@ module.exports = {
     once: true,
     async execute(client) {
         console.info(`Ready as ` + client.user.tag);
+        try {
+            client.user.setPresence({ activities: [{ name: 'The power of Aura' }], status: 'idle' });
+        }
+        catch (error) {
+            console.error(error.stack);
+        }
     },
 };
