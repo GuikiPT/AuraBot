8dd44a18c0172a75c5743528e08b641144e78c70
GuikiPT <foxzillapt17@gmail.com>
2023-03-02 15:44:25 +0000

    updated wrong variable name

---
+++ handlers/msgCommands.js
@@ -0,0 +1,24 @@
+// Node Modules
+const fs = require('fs');
+
+module.exports = async function (client) {
+    try {
+        /*
+            Created command handler similar than discord.js v12 command handling system
+            https://github.com/discordjs/guide/blob/v12/code-samples/command-handling/adding-features/12/index.js#L9-L17
+        */
+        const commandFolders = fs.readdirSync(__dirname + '/../commands/message/');
+    
+        for (const folder of commandFolders) {
+            const commandFiles = fs.readdirSync(__dirname + `/../commands/message/${folder}`).filter(file => file.endsWith('.js'));
+            for (const file of commandFiles) {
+                // https://discordjs.guide/creating-your-bot/command-handling.html#reading-command-files
+                const command = require(__dirname + `/../commands/message/${folder}/${file}`);
+                client.msgCommands.set(command.name, command);
+            }
+        }
+    }
+    catch (error) {
+        console.error(error.stack);
+    }
+}
