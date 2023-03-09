b13ae2c3e6b2bb030afeec7fc48145005cf7b35b
GuikiPT <foxzillapt17@gmail.com>
2023-03-09 09:36:57 +0000

    Created MessageCommand Handler Loader

--- handlers/msgCommands.js
+++ handlers/msgCommands.js
@@ -7,13 +7,13 @@ module.exports = async function (client) {
             Created command handler similar than discord.js v12 command handling system
             https://github.com/discordjs/guide/blob/v12/code-samples/command-handling/adding-features/12/index.js#L9-L17
         */
-        const commandFolders = fs.readdirSync(__dirname + '/../commands/message/');
+        const commandFolders = fs.readdirSync(__dirname + '/../commands/messages/');
     
         for (const folder of commandFolders) {
-            const commandFiles = fs.readdirSync(__dirname + `/../commands/message/${folder}`).filter(file => file.endsWith('.js'));
+            const commandFiles = fs.readdirSync(__dirname + `/../commands/messages/${folder}`).filter(file => file.endsWith('.js'));
             for (const file of commandFiles) {
                 // https://discordjs.guide/creating-your-bot/command-handling.html#reading-command-files
-                const command = require(__dirname + `/../commands/message/${folder}/${file}`);
+                const command = require(__dirname + `/../commands/messages/${folder}/${file}`);
                 client.msgCommands.set(command.name, command);
             }
         }
