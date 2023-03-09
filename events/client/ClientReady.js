dd3bc97885fbaac8ffd1d60a911f8f1ae5f7978e
GuikiPT <foxzillapt17@gmail.com>
2023-03-09 09:37:21 +0000

    idk

--- events/client/ClientReady.js
+++ events/client/ClientReady.js
@@ -10,6 +10,7 @@ const Discord = require('discord.js');
 module.exports = {
     name: Discord.Events.ClientReady,
     once: true,
+    
     async execute(client) {
         console.info(`Ready as ` + client.user.tag);
         try {
